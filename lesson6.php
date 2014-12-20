0c2e4e2a1e883f187975aea5a399f9411b1d050c
Nina Vlasova <ninadesigner@ya.ru>
2014-12-20 12:45:34 +0300

    lesson6

--- lesson6.php
+++ lesson6.php
@@ -1,5 +1,4 @@
 <?php
-
 error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 ini_set('display_errors', 1);
 header('Content-type: text/html; charset=utf-8');
@@ -7,240 +6,220 @@ header('Content-type: text/html; charset=utf-8');
 
 session_start();
 
-$_SESSION['db'][] = $_POST;
 
-// print_r($_SESSION['db']);
-
-
-//рисуем таблицу
-function show_table() {
+function show_table($show_id=false) {
     
-   echo '<table border = "1"> 
+    if (empty($_SESSION['bd'])) {
+        return;
+    }
+        if( $show_id!==false && !isset($_POST['main_form_submit'])){       
+             if (isset($_SESSION['bd'][$show_id])){
+                  show_item($show_id);
+            if (isset($_POST['main_form_submit'])) {
+                  header("Location: index.php");
+}
+   }
+        
+    } else {
+        
+    } 
+    
+    echo '<table border = "1"> 
    <tr>
    <td>Название объявления </td>
    <td>Цена</td>
    <td>Имя</td>
    <td>Удалить</td>
    </tr>';
-
-
-    foreach ($_SESSION['db'] as $id => $value) {
-        if (!empty($value['title']) & !empty($value['price']) & !empty($value['seller_name'])) {
+   
+   
+     foreach ($_SESSION['bd'] as $id => $value) {
+        if (empty($value['title']) & empty($value['price']) & empty($value['seller_name'])) {
+            continue;
+        }else {
             echo '<tr>
                 <td><a href=?action=show&id=' . $id . '>' . $value['title'] . '</td>
                 <td>' . $value['price'] . '</td>
                 <td>' . $value['seller_name'] . '</td>
                 <td><a href=?action=delete&id=' . $id . '>Удалить</td>                
-        </tr>';
-        } else {
-            continue;
-        }
-    }
-}
+                </tr>';
+     }        
+}          
+        echo '</table>';
+     }
 
-if (isset($_GET['action'])) {
-    switch ($_GET['action']) {
-        case 'delete':
-            $id = $_GET['id'];
-            if (isset($_SESSION['db'][$id])) {
-                unset($_SESSION['db'][$id]);
-            }
-            break;
-        case 'show':
-            $id = $_GET['id'];
-            if (isset($_SESSION['db'][$id])) {
-//                echo $id;
-            }
-            break;
-    }
-}
 
+     
 // обработчик формы
 
-function show_item() {
-    
-    $private = $_SESSION['db'][$_GET['id']]['private'];
-    $seller_name = $_SESSION['db'][$_GET['id']]['seller_name'];
-    $email = $_SESSION['db'][$_GET['id']]['email'];
-    $allow_mails = $_SESSION['db'][$_GET['id']]['allow_mails'];
-    $phone = $_SESSION['db'][$_GET['id']]['phone'];
-    $location_id = $_SESSION['db'][$_GET['id']]['location_id'];
-    $category_id = $_SESSION['db'][$_GET['id']]['category_id'];
-    $title = $_SESSION['db'][$_GET['id']]['title'];
-    $description = $_SESSION['db'][$_GET['id']]['description'];
-    $price = $_SESSION['db'][$_GET['id']]['price'];
-    
-        echo $private . '<br>
-            Ваше имя: ' . $seller_name . '<br>
-            Электронная почта: '.$email.'<br>'.$allow_mails.'<br>'.'
-            Номер телефона:'.$phone. '<br>
-            Город: '.$location_id. '<br>
-            Категория: '.$category_id.'<br>
-            Название объявления: '.$title.'<br>
-            Описание объявления: '.$description.'<br>
-            Цена: '.$price.'руб.';                         
-               
-              
-}
-
-
-    
-//print_r($_SESSION);  
-//show_item();
-//show_table();
+function show_item($id=FALSE) {
+  
 ?>
 
 
-
 <form  method="POST">
+    <head>
+        <style>
+.my_button {
+  display: inline-block;
+  width: 10em;
+  font-size: 80%;
+  color: rgba(255,255,255,.9);
+  text-shadow: #2e7ebd 0 1px 2px;
+  text-decoration: none;
+  text-align: center;
+  line-height: 1.1;
+  white-space: pre-line;
+  padding: .6em 0;
+  border: 1px solid;
+  border-color: #60a3d8 #2970a9 #2970a9 #60a3d8;
+  border-radius: 6px;
+  outline: none;
+  background: #60a3d8 linear-gradient(#89bbe2, #60a3d8 50%, #378bce);
+  box-shadow: inset rgba(255,255,255,.5) 1px 1px;
 
+}
+</style>
+    </head>
     <div class="form-row-indented"> 
+    
+       <?php 
+                $checkbox = 'checked=""';
+                 
+                $company = "";
+                $private = "";
+                
+                if(isset($_SESSION['bd'][$id]['private']) )
+                {
+                    if($_SESSION['bd'][$id]['private']==1) {
+                        $private = $checkbox;                      
+                    }else{
+                        $company = $checkbox;
+                    }       
+                    
+                }else{
+                    
+                    $private = $checkbox;
+                }
+                
+       ?>
+   
         <label class="form-label-radio">
-            <input type="radio" checked="" value="1" name="private">Частное лицо</label> 
+            <input type="radio" <?=$private?> value="1" name="private">Частное лицо</label> 
+            
         <label class="form-label-radio">
-            <input type="radio" value="0" name="private">Компания</label> </div>
+            <input type="radio" <?=$company?> value="0" name="private">Компания</label> </div>
+            
+            
+            
     <div class="form-row"> 
         <label for="fld_seller_name" class="form-label"><b id="your-name">Ваше имя</b></label>
-        <input type="text" maxlength="40" class="form-input-text" value="" name="seller_name" id="fld_seller_name">
-
+        <input type="text" maxlength="40" class="form-input-text" value="<?php  if(isset($_SESSION['bd'][$id]['seller_name']))echo $_SESSION['bd'][$id]['seller_name'] ?>" name="seller_name" id="fld_seller_name">
     </div>
+    
+    
     <div class="form-row"> 
         <label for="fld_email" class="form-label">Электронная почта</label>
-        <input type="text" class="form-input-text" value="" name="email" id="fld_email">
+        <input type="text" class="form-input-text" value="<?php  if(isset($_SESSION['bd'][$id]['email']))echo $_SESSION['bd'][$id]['email'] ?>" name="email" id="fld_email">
     </div>
+    
+    
     <div class="form-row-indented"> 
         <label class="form-label-checkbox" for="allow_mails"> 
-            <input type="checkbox" value="1" name="allow_mails" id="allow_mails" class="form-input-checkbox">
+        
+        <?php
+            $checkbox = '';
+                if(!empty($_SESSION['bd'][$id]['allow_mails'])){
+                        $checkbox = 'checked=""';
+                }               
+            ?> 
+        
+            <input type="checkbox" <?php echo $checkbox ?> value="1" name="allow_mails" id="allow_mails" class="form-input-checkbox">
             <span class="form-text-checkbox">Я не хочу получать вопросы по объявлению по e-mail</span> 
         </label> </div>
+        
+        
     <div class="form-row"> 
         <label id="fld_phone_label" for="fld_phone" class="form-label">Номер телефона</label> 
-        <input type="text" class="form-input-text" value="" name="phone" id="fld_phone">
+        <input type="text" class="form-input-text" value="<?php  if(isset($_SESSION['bd'][$id]['phone']))echo $_SESSION['bd'][$id]['phone'] ?>" name="phone" id="fld_phone">
 
 
     </div>
     <div id="f_location_id" class="form-row form-row-required"> 
         <label for="region" class="form-label">Город</label> 
+        
+        
+        
+        <?php 
+            
+            $citys = array('641780'=>'Новосибирск','641490'=>'Барабинск','641510'=>'Бердск');
+            if(isset($_SESSION['bd'][$id]['location_id'])){
+            $gorod = $_SESSION['bd'][$id]['location_id'];
+                }
+        ?>
+        
+        
+        
         <select title="Выберите Ваш город" name="location_id" id="region" class="form-input-select"> 
             <option value="">-- Выберите город --</option>
-            <option class="opt-group" disabled="disabled">-- Города --</option>
-            <option selected="" data-coords=",," value="641780">Новосибирск</option>   
-            <option data-coords=",," value="641490">Барабинск</option>   
-            <option data-coords=",," value="641510">Бердск</option>   
-            <option data-coords=",," value="641600">Искитим</option>   
-            <option data-coords=",," value="641630">Колывань</option>   
-            <option data-coords=",," value="641680">Краснообск</option>   
-            <option data-coords=",," value="641710">Куйбышев</option>   
-            <option data-coords=",," value="641760">Мошково</option>   
-            <option data-coords=",," value="641790">Обь</option>   
-            <option data-coords=",," value="641800">Ордынское</option>   
-            <option data-coords=",," value="641970">Черепаново</option>   
-            <option id="select-region" value="0">Выбрать другой...</option> 
+        
+        <?php
+            foreach($citys as $number=>$city){
+                $selected = ($number==$gorod) ? 'selected=""' : '';
+                echo '<option data-coords=",," '.$selected.' value="'.$number.'">'.$city.'</option>'; 
+            }
+        ?>  
         </select> 
-
+        
+        
+        
+        
         <div class="form-row"> 
             <label for="fld_category_id" class="form-label">Категория</label> 
+            
+            <?php 
+            $categoryes = array('09'=>'Новые автомобили', '05'=>'Квартиры', '26'=>'Земельные участки', '86'=>'Недвижимость за рубежом', '19'=>'Ремонт и строительство', '21'=>'Бытовая техника');
+                if(isset($_SESSION['bd'][$id]['category_id'])){
+            $name = $_SESSION['bd'][$id]['category_id'];
+            }
+        ?>
+        
+        
             <select title="Выберите категорию объявления" name="category_id" id="fld_category_id" class="form-input-select"> 
                 <option value="">-- Выберите категорию --</option>
-                <optgroup label="Транспорт">
-                    <option value="9">Автомобили с пробегом</option>
-                    <option value="109">Новые автомобили</option>
-                    <option value="14">Мотоциклы и мототехника</option>
-                    <option value="81">Грузовики и спецтехника</option>
-                    <option value="11">Водный транспорт</option>
-                    <option value="10">Запчасти и аксессуары</option>
-                </optgroup>
-                <optgroup label="Недвижимость">
-                    <option value="24">Квартиры</option>
-                    <option value="23">Комнаты</option>
-                    <option value="25">Дома, дачи, коттеджи</option>
-                    <option value="26">Земельные участки</option>
-                    <option value="85">Гаражи и машиноместа</option>
-                    <option value="42">Коммерческая недвижимость</option>
-                    <option value="86">Недвижимость за рубежом</option>
-                </optgroup>
-                <optgroup label="Работа">
-                    <option value="111">Вакансии (поиск сотрудников)</option>
-                    <option value="112">Резюме (поиск работы)</option>
-                </optgroup>
-                <optgroup label="Услуги">
-                    <option value="114">Предложения услуг</option>
-                    <option value="115">Запросы на услуги</option>
-                </optgroup>
-                <optgroup label="Личные вещи">
-                    <option value="27">Одежда, обувь, аксессуары</option>
-                    <option value="29">Детская одежда и обувь</option>
-                    <option value="30">Товары для детей и игрушки</option>
-                    <option value="28">Часы и украшения</option>
-                    <option value="88">Красота и здоровье</option>
-                </optgroup>
-                <optgroup label="Для дома и дачи">
-                    <option value="21">Бытовая техника</option>
-                    <option value="20">Мебель и интерьер</option>
-                    <option value="87">Посуда и товары для кухни</option>
-                    <option value="82">Продукты питания</option>
-                    <option value="19">Ремонт и строительство</option>
-                    <option value="106">Растения</option>
-                </optgroup>
-                <optgroup label="Бытовая электроника">
-                    <option value="32">Аудио и видео</option>
-                    <option value="97">Игры, приставки и программы</option>
-                    <option value="31">Настольные компьютеры</option>
-                    <option value="98">Ноутбуки</option>
-                    <option value="99">Оргтехника и расходники</option>
-                    <option value="96">Планшеты и электронные книги</option>
-                    <option value="84">Телефоны</option>
-                    <option value="101">Товары для компьютера</option>
-                    <option value="105">Фототехника</option>
-                </optgroup>
-                <optgroup label="Хобби и отдых">
-                    <option value="33">Билеты и путешествия</option>
-                    <option value="34">Велосипеды</option>
-                    <option value="83">Книги и журналы</option>
-                    <option value="36">Коллекционирование</option>
-                    <option value="38">Музыкальные инструменты</option>
-                    <option value="102">Охота и рыбалка</option>
-                    <option value="39">Спорт и отдых</option>
-                    <option value="103">Знакомства</option>
-                </optgroup>
-                <optgroup label="Животные">
-                    <option value="89">Собаки</option>
-                    <option value="90">Кошки</option>
-                    <option value="91">Птицы</option>
-                    <option value="92">Аквариум</option>
-                    <option value="93">Другие животные</option>
-                    <option value="94">Товары для животных</option>
-                </optgroup>
-                <optgroup label="Для бизнеса">
-                    <option value="116">Готовый бизнес</option>
-                    <option value="40">Оборудование для бизнеса</option>
-                </optgroup>
-            </select> 
+           
+        
+        <?php
+                
+                    foreach($categoryes as $number=>$category){
+                        $selected = ($number==$name) ? 'selected=""' : '';
+                        echo '<option data-coords=",," '.$selected.' value="'.$number.'">'.$category.'</option>';
+                    }
+                    
+                ?>
+         </select> 
         </div>
 
-        <div style="display: none;" id="params" class="form-row form-row-required"> 
-            <label class="form-label ">
-                Выберите параметры
-            </label> <div class="form-params params" id="filters">
-            </div> 
+        <div id="f_title" class="form-row f_title"> <label for="fld_title" class="form-label">Название объявления</label> <input type="text" maxlength="50" class="form-input-text-long" value="<?php  if(isset($_SESSION['bd'][$id]['title']))echo $_SESSION['bd'][$id]['title'] ?>" name="title" id="fld_title"> 
         </div>
-
-        <div id="f_title" class="form-row f_title"> <label for="fld_title" class="form-label">Название объявления</label> <input type="text" maxlength="50" class="form-input-text-long" value="" name="title" id="fld_title"> 
-        </div>
+        
+        
         <div class="form-row"> 
             <label for="fld_description" class="form-label" id="js-description-label">Описание объявления</label> 
-            <textarea maxlength="3000" name="description" id="fld_description" class="form-input-textarea"></textarea> 
+            <textarea maxlength="3000" name="description" id="fld_description" class="form-input-textarea"><?php  if(isset($_SESSION['bd'][$id]['description'])) echo $_SESSION['bd'][$id]['description'] ?></textarea> 
         </div>
+        
+        
         <div id="price_rw" class="form-row rl"> 
             <label id="price_lbl" for="fld_price" class="form-label">Цена</label> 
-            <input type="text" maxlength="9" class="form-input-text-short" value="0" name="price" id="fld_price">&nbsp;
+            <input type="text" maxlength="9" class="form-input-text-short" value="<?php  if(isset($_SESSION['bd'][$id]['fld_price']))echo $_SESSION['bd'][$id]['fld_price'] ?>" name="price" id="fld_price">&nbsp;
             <span id="fld_price_title">руб.</span> 
 
             <div class="form-row-indented form-row-submit b-vas-submit" id="js_additem_form_submit">
                 <div class="vas-submit-button pull-left"> 
                     <span class="vas-submit-border"></span> 
                     <span class="vas-submit-triangle"></span> 
-                    <input type="submit" value="Отправить" id="form_submit" name="main_form_submit" class="vas-submit-input"> 
+                    <input type="submit" value="Отправить" id="form_submit" name="main_form_submit" class="my_button"vas-submit-input"> 
                 </div>
             </div>
             </form>
@@ -248,9 +227,45 @@ function show_item() {
 
 
 <?php
+}
+
+
+
+if (isset($_POST['main_form_submit'])) {
+    if (!empty($_POST['title']) || (!empty($_POST['price']) || !empty($_POST['seller_name']))) {
+        if (isset($_GET['id'])) {
+            $_SESSION['bd'][$_GET['id']] = $_POST;
+        } else {
+            $_SESSION['bd'][] = $_POST;
+        }
+        // header("Location:index.php");
+        
+    }
+}
+
+if (isset($_GET['action'])) {
+    switch ($_GET['action']) {
+        case 'delete':
+            $id = $_GET['id'];
+            if (isset($_SESSION['bd'][$id])) {
+                unset($_SESSION['bd'][$id]);
+                // header("Location:index.php");
+            }
+            break;
+        case 'show':
+            $id = $_GET['id'];
+            if (isset($_SESSION['bd'][$id])) {
+                  show_table($id);
+                  exit;
+
+            }
+            break;
+    }
+}
 
 show_table();
+echo '<br>';
 show_item();
 
-
+// session_destroy();
 ?>
